46a182698b6bf03a94e7abb1a0c1e7ff2bc77e13
dody <dody@bigpearl.io>
2022-11-10 17:54:05 +0900

    interpolation 테스트 추가

---
+++ src/chapter2/AnimatedInterpolation.js
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react'
+import { Animated, View, Text, Button } from 'react-native'
+
+// 가로 세로 30, scale 너비 1 > 2
+export default function AnimatedInterpolate() {
+  const scaleAnim = useRef(new Animated.Value(1)).current
+
+  const onButtonPress = () => {
+    Animated.timing(scaleAnim, {
+      toValue: 2,
+      useNativeDriver: false,
+    }).start()
+  }
+
+  return (
+    <>
+      <Button title="보간법 실험" onPress={onButtonPress} />
+      <Animated.View
+        style={{
+          width: scaleAnim.interpolate({
+            inputRange: [1, 2],
+            outputRange: [30, 60],
+          }),
+          height: 30,
+          backgroundColor: scaleAnim.interpolate({
+            inputRange: [1, 2],
+            outputRange: ['#333', '#aff100'],
+          }),
+          transform: [
+            { scale: scaleAnim },
+            {
+              rotate: scaleAnim.interpolate({
+                inputRange: [1, 1.7, 2],
+                outputRange: ['0deg', '450deg', '360deg'],
+              }),
+            },
+          ],
+        }}
+      />
+    </>
+  )
+}
