993559e3c2155515d9cd5045e836718b082c4f8a
dody <dody@bigpearl.io>
2022-11-13 15:47:26 +0900

    zz

--- src/chapter2/AnimatedInterpolation.js
+++ src/chapter2/AnimatedInterpolation.js
@@ -4,10 +4,16 @@ import { Animated, View, Text, Button } from 'react-native'
 // 가로 세로 30, scale 너비 1 > 2
 export default function AnimatedInterpolate() {
   const scaleAnim = useRef(new Animated.Value(1)).current
+  const widthdd = useRef(new Animated.Value(30)).current
 
   const onButtonPress = () => {
     Animated.timing(scaleAnim, {
       toValue: 2,
+      useNativeDriver: false,
+    }).start()
+
+    Animated.timing(widthdd, {
+      toValue: 60,
       useNativeDriver: false,
     }).start()
   }
@@ -17,10 +23,7 @@ export default function AnimatedInterpolate() {
       <Button title="보간법 실험" onPress={onButtonPress} />
       <Animated.View
         style={{
-          width: scaleAnim.interpolate({
-            inputRange: [1, 2],
-            outputRange: [30, 60],
-          }),
+          width: widthdd,
           height: 30,
           backgroundColor: scaleAnim.interpolate({
             inputRange: [1, 2],
