efee26a7d9b6e85181e1a190b29e7326f6b2c105
dody <dody@bigpearl.io>
2022-11-26 12:58:38 +0900

    zz

--- App.js
+++ App.js
@@ -1,50 +1,50 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import React from 'react'
+import { SafeAreaView, StyleSheet, View } from 'react-native'
 
-import AnimatedValue from "./src/chapter2/02AnimatedValue/AnimatedValue";
-import AnimatedSpring from "./src/chapter2/AnimatedSpring";
-import AnimatedDecay from "./src/chapter2/05animatedDecay/AnimatedDecay";
+import AnimatedValue from './src/chapter2/02AnimatedValue/AnimatedValue'
+import AnimatedSpring from './src/chapter2/AnimatedSpring'
+import AnimatedDecay from './src/chapter2/05animatedDecay/AnimatedDecay'
 // import AnimatedCombining from "./src/chapter2/06animatedCombining/AnimatedCombining";
-import AnimatedCombining from "./src/chapter2/Combining";
-import AnimatedOtherMethod from "./src/chapter2/AnimatedOtherMethod";
-import AnimatedComposing from "./src/chapter2/AnimatedComposing";
+import AnimatedCombining from './src/chapter2/Combining'
+import AnimatedOtherMethod from './src/chapter2/AnimatedOtherMethod'
+import AnimatedComposing from './src/chapter2/AnimatedComposing'
 
-import AnimatedProperty from "./src/chapter2/07animatedInterpolation/AnimatedProperty";
-import AnimatedInterpolation from "./src/chapter2/AnimatedInterpolation";
+import AnimatedProperty from './src/chapter2/07animatedInterpolation/AnimatedProperty'
+import AnimatedInterpolation from './src/chapter2/AnimatedInterpolation'
 
-import Snackbar from "./src/chapter3/01snackbar/Snackbar";
-import SnackbarAnimation from "./src/chapter3/01snackbar/SnackbarAnimation";
-import DrawerMenu from "./src/chapter3/02drawerMenu/DrawerMenu";
-import AnimatedCollapse from "./src/chapter3/03collapse/AnimatedCollapse";
-import Collapse from "./src/chapter3/03collapse/Collapse";
-import Progressbar from "./src/chapter3/Progressbar";
-import ProgressbarAnimation from "./src/chapter3/ProgressbarAnimation";
-import Skeleton from "./src/chapter3/Skeleton";
-import SnowBackground from "./src/chapter3/SnowBackground";
+import Snackbar from './src/chapter3/01snackbar/Snackbar'
+import SnackbarAnimation from './src/chapter3/01snackbar/SnackbarAnimation'
+import DrawerMenu from './src/chapter3/02drawerMenu/DrawerMenu'
+import AnimatedCollapse from './src/chapter3/03collapse/AnimatedCollapse'
+import Collapse from './src/chapter3/03collapse/Collapse'
+import Progressbar from './src/chapter3/Progressbar'
+import ProgressbarAnimation from './src/chapter3/ProgressbarAnimation'
+import Skeleton from './src/chapter3/Skeleton'
+import SnowBackground from './src/chapter3/SnowBackground'
 
-import LayoutAnimationIntro from "./src/chapter4/LayoutAnimationIntro";
-import LayoutAnimationPageHeader from "./src/chapter4/LayoutAnimationPageHeader";
-import LayoutAnimationCollapse from "./src/chapter4/LayoutAnimationCollapse";
+import LayoutAnimationIntro from './src/chapter4/LayoutAnimationIntro'
+import LayoutAnimationPageHeader from './src/chapter4/LayoutAnimationPageHeader'
+import LayoutAnimationCollapse from './src/chapter4/LayoutAnimationCollapse'
 
-import InteractionManagerIntro from "./src/chapter5/InteractionManagerBasic";
+import InteractionManagerIntro from './src/chapter5/InteractionManagerBasic'
 
-import PanresponderIntro from "./src/chapter6/PanresponderIntro";
+import PanresponderIntro from './src/chapter6/PanresponderIntro'
 // import PanresponderBasic from "./src/chapter6/PanresponderBasic";
-import PanresponderBall from "./src/chapter6/PanresponderBall";
+import PanresponderBall from './src/chapter6/PanresponderBall'
 // ! animted.addListener  로 데이터를 확인할 수 있을 것!
 // import PanresponderBox from "./src/chapter6/PanresponderBox";
-import PanresponderModalView from "./src/chapter6/PanresponderModalView";
-import PanresponderModalAnimation from "./src/chapter6/PanresponderModalAnimation";
-import PanresponderBannerSlider from "./src/chapter6/PanresponderBannerSlider";
-import PanresponderFontSlider from "./src/chapter6/PanresponderFontSlider";
+import PanresponderModalView from './src/chapter6/PanresponderModalView'
+import PanresponderModalAnimation from './src/chapter6/PanresponderModalAnimation'
+import PanresponderBannerSlider from './src/chapter6/PanresponderBannerSlider'
+import PanresponderFontSlider from './src/chapter6/PanresponderFontSlider'
 
 const App = () => {
   return (
     <View style={styles.wrapper}>
-      <PanresponderIntro />
+      <PanresponderBall />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -53,6 +53,6 @@ const styles = StyleSheet.create({
     // alignItems: "center",
     // backgroundColor: "#ffa100",
   },
-});
+})
 
-export default App;
+export default App
--- src/chapter6/PanresponderBall.js
+++ src/chapter6/PanresponderBall.js
@@ -1,49 +1,45 @@
-import React, { useRef, useState } from "react";
-import { View, Text, Animated, PanResponder } from "react-native";
+import React, { useRef, useState } from 'react'
+import { View, Text, Animated, PanResponder } from 'react-native'
 
 export default function PanresponderBall() {
-  const WIDTH = 100;
-  const panAnim = useRef(new Animated.ValueXY(0)).current;
+  const panAnim = useRef(new Animated.ValueXY(0)).current
 
   const panResponder = PanResponder.create({
     onMoveShouldSetPanResponder: (evt, gestureState) => true,
-    // todo: panresponder에서 event의 사용 방법
     onPanResponderMove: Animated.event(
       [null, { dx: panAnim.x, dy: panAnim.y }],
       {
         useNativeDriver: false,
-        listener: (e, gestureState) => console.log(gestureState.moveX),
-      }
+      },
     ),
 
     // 터치 이벤트가 끝났을 떄
     onPanResponderEnd: (evt, gestureState) => {
-      console.log("onPanResponderEnd", gestureState);
       Animated.decay(panAnim, {
-        velocity: { x: gestureState.vx, y: gestureState.vy },
-        deceleration: 0.999,
+        velocity: { x: gestureState.vx / 50, y: gestureState.vy / 50 },
+        deceleration: 0.998,
         useNativeDriver: true,
-      }).start();
+      }).start()
     },
 
     onPanResponderRelease: (evt, gestureState) => {
       setTimeout(() => {
-        panAnim.setValue({ x: 0, y: 50 });
+        panAnim.setValue({ x: 0, y: 50 })
         Animated.spring(panAnim, {
           toValue: { x: 0, y: 0 },
           duration: 1000,
           useNativeDriver: true,
-        }).start();
-      }, 1000);
+        }).start()
+      }, 1000)
     },
-  });
+  })
 
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Animated.View
@@ -52,7 +48,7 @@ export default function PanresponderBall() {
         // 구조분해할당
         style={{
           transform: [{ translateX: panAnim.x }, { translateY: panAnim.y }],
-          position: "absolute",
+          position: 'absolute',
           bottom: 20,
           borderRadius: 100,
         }}
@@ -60,5 +56,5 @@ export default function PanresponderBall() {
         <Text style={{ fontSize: 100 }}>🏀</Text>
       </Animated.View>
     </View>
-  );
+  )
 }
